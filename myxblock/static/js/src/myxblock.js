4dfce6b2c4c070b77c819602096555cb42ba5bbe
J4ckDev <j4ckdev@gmail.com>
2020-12-03 10:03:17 -0500

    Vistas modificadas

--- myxblock/static/js/src/myxblock.js
+++ myxblock/static/js/src/myxblock.js
@@ -1,26 +1,17 @@
 /* Javascript for MyXBlock. */
 function MyXBlock(runtime, element) {
     
-    function updateData(result) {
-        var sectionData = document.getElementById("data");
-        sectionData.innerHTML = result.resultado;
-	    document.getElementById("name").value = "";
-        document.getElementById("lastname").value = "";
-        document.getElementById("email").value = "";
-    }
-
     var handlerUrl = runtime.handlerUrl(element, 'get_formdata');
 
-    $('#Send', element).click(function (eventObject) {
+        $('#Send', element).click(function (eventObject) {
         var name = document.getElementById("name").value;
         var lastname = document.getElementById("lastname").value;
         var email = document.getElementById("email").value;
-
         $.ajax({
-	        type: "POST",
-            url: handlerUrl,            
+            type: "POST",
+            url: handlerUrl,                     
             data: JSON.stringify({ "name": name, "lastname": lastname, "email": email }),
-            success: updateData
+            success: location.reload
         });
     });
 
